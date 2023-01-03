d233625254f709ca93145b82f5acde0308f8b0da
Asmaa Belhadj <abelhadj@studenti.uninsubria.it>
2023-01-03 14:55:26 +0100

    SpringBoot

--- EsercitazioneSpringBoot/src/main/java/com/esercitazione/esercitazionespringboot/model/Course.java
+++ EsercitazioneSpringBoot/src/main/java/com/esercitazione/esercitazionespringboot/model/Course.java
@@ -2,44 +2,36 @@ package com.esercitazione.esercitazionespringboot.model;
 
 
 import jakarta.persistence.*;
+import lombok.*;
 
 
 @Entity
 @Table(name = "course")
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Course {
     @Id
+    @Getter
+    @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+
+
+    @Getter
+    @Setter
     @Column(name = "name_course")
     private String name_course;
+
+
+
+
+    @Getter
+    @Setter
     @Column(name = "description")
     private String description;
 
-    public Course() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName_course() {
-        return name_course;
-    }
-
-    public void setName_course(String name_course) {
-        this.name_course = name_course;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
 
